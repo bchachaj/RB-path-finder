7d4f198833a34c865c48f24bd70df598c8786b46
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-07-07 14:22:14 -0400

    Added bfs and dfs instance methods to PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   attr_accessor :value
 
@@ -22,20 +24,54 @@ class PolyTreeNode
       parent.children.delete(self) unless parent == nil
       @parent = node
       parent.children << self unless parent.children.include?(self)
-
     end
   end
 
 
-  def add_children(child_node)
+  def add_child(child_node)
+    child_node.parent = self
 
   end
 
 
   def remove_child(child_node)
+    # raise "Error" if @children.include?(child_node)
+    child_node.parent = nil
+    raise "Error" if !(@children.include?(child_node))
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    @children.each do |child|
+
+      search_result = child.dfs(target_value)
+
+      return search_result unless search_result.nil?
+    end
+    nil
 
   end
 
 
+
+  def bfs(target_value)
+    queue = [self]
+
+    until queue.empty?
+
+      current_node = queue.shift
+
+
+      if current_node.value == target_value
+        return current_node
+      else
+        current_node.children.each {|child| queue << child}
+
+      end
+
+    end
+    nil
+  end
 
 end
